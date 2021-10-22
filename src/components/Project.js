93eeded61fd9c3b4ff13ce18921ad04301e967bb
yinping-520 <85038945+yinping-520@users.noreply.github.com>
2021-10-21 22:37:05 -0400

    github repo added

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,14 +1,16 @@
 import React from "react";
 import choredoor from '../img/chore-door.png'
 import recipebook from '../img/recipebook.png'
-import rps from '../img/rock-paper-scissor-img.png'
+import music from '../img/genre.png'
 import weather from '../img/weatherdashboard.png'
+
+
 
 
 
 const styles ={
     main:{
-        height:650,
+        height:1500,
         background:'DarkSlateBlue'
     },
     bodyStyle:{
@@ -16,7 +18,11 @@ const styles ={
     },
     imgStyle:{
         width: "auto",
-        height: 250
+        height: 250,
+
+        "&:hover": {
+            background: 'black'
+          },
         
     }
 }
@@ -27,24 +33,51 @@ function Project(){
         <main style={styles.main}>
             <div className="row row-cols-1 row-cols-md-2 g-4" style={styles.bodyStyle}>
                 <div className="col">
+                <a href="https://yinping-520.github.io/Chore-Door/">
                   <div className="card">
-                    <img src={choredoor} className="card-img-top" alt="chore door" style={styles.imgStyle}/>
+                        <img src={choredoor} className="card-img-top" alt="chore door" style={styles.imgStyle}/>
+                        <div class="card-body">
+                            <a href="https://github.com/yinping-520/Chore-Door">
+                                <h5 class="card-title">GitHub Repo</h5>
+                            </a>
+                        </div>
                   </div>
+                </a>
                 </div>
                 <div className="col">
+                  <a href="https://yinping-520.github.io/recipe-in-hand/">
                   <div className="card">
                     <img src={recipebook} className="card-img-top" alt="recipe book" style={styles.imgStyle}/>
+                    <div class="card-body">
+                        <a href="https://github.com/yinping-520/recipe-in-hand">
+                            <h5 class="card-title">GitHub Repo</h5></a>
+                    </div>
                   </div>
+                  </a>
                 </div>
                 <div className="col">
+                  <a href="https://eom-10-4.herokuapp.com/login">  
                   <div className="card">
-                    <img src={rps} className="card-img-top" alt="rock paper scissors" style={styles.imgStyle}/>
+                    <img src={music} className="card-img-top" alt="essence of music" style={styles.imgStyle}/>
+                    <div class="card-body">
+                        <a href="https://github.com/yinping-520/Essence_Of_Music">
+                            <h5 class="card-title">GitHub Repo</h5>
+                        </a>
+                    </div>
                   </div>
+                  </a>
                 </div>
                 <div className="col">
+                  <a href="https://yinping-520.github.io/weather-dashboard/">
                   <div className="card">
                     <img src={weather} className="card-img-top" alt="weather dashboard" style={styles.imgStyle}/>
+                    <div class="card-body">
+                        <a href="https://github.com/yinping-520/weather-dashboard">
+                            <h5 class="card-title">GitHub Repo</h5>
+                        </a>
+                    </div>
                   </div>
+                  </a>
                 </div>
             </div>
 
